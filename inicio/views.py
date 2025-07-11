d95ca09467fba63b63269e30cd2777ff5c2d6f7e
LuisMox <luis.l.a.z.g.11.18@gmail.com>
2025-07-10 18:22:29 -0600

    vista tareas funciona

--- inicio/views.py
+++ inicio/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def principal(request):
     return render(request,"inicio/principal.html")
+
+def tareas(request):
+    return render(request,"inicio/tareas.html")
--- jardinDeNiños/urls.py
+++ jardinDeNiños/urls.py
@@ -20,5 +20,6 @@ from inicio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.principal, name="principal")
+    path('',views.principal, name="principal"),
+    path('tareas/',views.tareas, name="tareas")
 ]
