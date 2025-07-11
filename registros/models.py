69bc92808d0ffa6d75cea408d206eb6eaafd983f
LuisMox <luis.l.a.z.g.11.18@gmail.com>
2025-07-11 17:52:20 -0600

    Modelo Alumno

---
+++ registros/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+#<Luis alejandro Zacarias gonzalez
+class Alumnos(models.Model): #Define la estructura de nuestra tabla
+
+    ident = models.CharField(max_length=12, verbose_name="Identificacion")
+    nombre = models.TextField(verbose_name="Nombre") 
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creado") #Fecha y tiempo
+    updated = models.DateTimeField(auto_now_add=True, verbose_name="Actualizado")
+
+    class Meta:
+        verbose_name="Alumno"
+        verbose_name_plural = "Alumnos"
+        ordering = ["-created"]
+    def __str__(self):
+        return self.nombre
+
+#/>
