c2370f9befb952332ed540a3800dd36736894d11
luis_villela <luisvillela110@gmail.com>
2025-07-11 00:45:38 -0600

    srrlego

--- inicio/views.py
+++ inicio/views.py
@@ -16,11 +16,3 @@ def reconocimientos(request):
     return render(request,"inicio/reconocimientos.html")
 
 
-def reconocimientos_view(request):
-    reconocimientos_del_usuario = Reconocimiento.objects.filter(usuario=request.user).order_by('-fecha_otorgado')
-    
-    contexto = {
-        'reconocimientos': reconocimientos_del_usuario
-    }
-    
-    return render(request, 'inicio/reconocimientos.html', contexto)
