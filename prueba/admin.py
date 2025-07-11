68d4d8e7ab131ae7f14681ba39ac6614ce7c23b6
luis_villela <luisvillela110@gmail.com>
2025-07-11 00:34:34 -0600

    modelo

--- prueba/admin.py
+++ prueba/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import Reconocimiento
-# Register your models here.
 
-@admin.register(Reconocimiento)
-class ReconocimientoAdmin(admin.ModelAdmin):
-    
-    list_display = ('titulo', 'usuario', 'fecha_otorgado')
-    list_filter = ('usuario', 'fecha_otorgado')
-    search_fields = ('titulo', 'descripcion')
+# Register your models here.
