aadd79d7961ec23631cff4311f120f45f4d1c4ff
luis_villela <luisvillela110@gmail.com>
2025-07-10 23:40:46 -0600

    apartada  y modelos reconocimientos

--- prueba/admin.py
+++ prueba/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Reconocimiento
 # Register your models here.
+
+@admin.register(Reconocimiento)
+class ReconocimientoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'usuario', 'fecha_otorgado')
+    list_filter = ('fecha_otorgado', 'usuario')
+    search_fields = ('titulo', 'descripcion', 'usuario__username')
