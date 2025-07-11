f7ce86076fe1c08236c062528b8a96b58d2b67fd
MagicDaniel-XL3 <homitochuleta@gmail.com>
2025-07-11 00:38:48 -0600

    models py error

--- inicio/models.py
+++ inicio/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
-#aaa
+
+class Alumnos(models.Model):  # define la estructura de nuestra tabla
+    nombre = models.Charfield(max_length=12)  
+    imagen = models.ImageField(null=True, upload_to="fotos", verbose_name="Fotografia")
+    grado = models.Charfield(max_length=10)
+    grupo = models.Charfield(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)  # Fecha y Hora
+    updated = models.DateTimeField(auto_now=True)  # Se actualiza al guardar cambios
