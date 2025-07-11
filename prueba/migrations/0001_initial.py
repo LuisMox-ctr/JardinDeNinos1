3f0256e60783144006052af1989da49771b61e9a
luis_villela <luisvillela110@gmail.com>
2025-07-11 01:59:01 -0600

    m

--- prueba/migrations/0001_initial.py
+++ prueba/migrations/0001_initial.py
@@ -13,16 +13,4 @@ class Migration(migrations.Migration):
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
-    operations = [
-        migrations.CreateModel(
-            name='Reconocimiento',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('titulo', models.CharField(max_length=200)),
-                ('descripcion', models.TextField(blank=True, null=True)),
-                ('imagen', models.ImageField(upload_to='reconocimientos/')),
-                ('fecha_otorgado', models.DateField()),
-                ('usuario', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='reconocimientos', to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-    ]
+    
