4b6187b9b1f15f847892e2ee30f47910495539d1
LuisMox <luis.l.a.z.g.11.18@gmail.com>
2025-07-11 18:55:34 -0600

    modelo de tareas

--- registros/admin.py
+++ registros/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Alumnos
+from .models import Tareas
 # Register your models here.
 #<Luis alejandro Zacarias gonzalez
 class AdministrarModelo(admin.ModelAdmin):
     readonly_fields=('created','updated')
 
 admin.site.register(Alumnos, AdministrarModelo)
+
+class AdministrarTareas(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+admin.site.register(Tareas)
--- registros/models.py
+++ registros/models.py
@@ -16,4 +16,15 @@ class Alumnos(models.Model):
     def __str__(self):
         return self.nombre
 
-#/>
+class Tareas(models.Model):
+    idtarea = models.CharField(max_length=10, verbose_name="Numero de tarea")
+    nombre = models.TextField(verbose_name="Nombre")
+    descripcion= models.TextField(verbose_name="Descripcion")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creado") 
+    updated = models.DateTimeField(auto_now_add=True, verbose_name="Actualizado")
+    class Meta:
+        verbose_name="Tarea"
+        verbose_name_plural="Tareas"
+        ordering=["-created"]
+    def __str__(self):
+        return self.nombre
