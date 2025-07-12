2eb9adfbdfb5f11b67451c4bd7df09b5cd2b037a
luis_villela <luisvillela110@gmail.com>
2025-07-11 20:20:17 -0600

    recientes

--- inicio/views.py
+++ inicio/views.py
@@ -16,7 +16,6 @@ def dudas(request):
 def reconocimientos(request):
     return render(request, "inicio/reconocimientos.html")
 
-def recientes(request):
-    return render(request, "inicio/recientes.html")
+
 
 #prueba
