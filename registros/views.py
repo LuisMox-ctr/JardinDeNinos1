7201cfdb3f817a5a11b171e5003875980d51cfee
LuisMox <luis.l.a.z.g.11.18@gmail.com>
2025-08-18 01:28:39 -0600

    se añadio filtros por alumno en la vista principal de registros junto con otra logica para views

--- registros/views.py
+++ registros/views.py
@@ -4,20 +4,57 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.contrib import messages
 from .forms import EntregarTareaForm
+from django.db.models import Q
 
 # Create your views here.
 def registros(request):
     # Si llega un filtro por GET, lo usamos
-    filtro_estado = request.GET.get("estado", "todas")  
+    filtro_estado = request.GET.get("estado", "todas")
+    alumno_id = request.GET.get("alumno","")
+    q = request.GET.get("q", "").strip()
+    
+    asignaciones = (AlumnoTarea.objects
+        .select_related('alumno', 'tarea')
+        .all())
 
-    if filtro_estado == "pendiente":
+    # Filtro por estado
+    if filtro_estado in ("pendiente","terminada"):
+        asignaciones = asignaciones.filter(estado=filtro_estado)
+        # Filtro por alumno (id exacto)
+    if alumno_id:
+        asignaciones = asignaciones.filter(alumno_id=alumno_id)
+         # Búsqueda libre (nombre o ident)
+    if q:
+        asignaciones = asignaciones.filter(
+            Q(alumno__nombre__icontains=q) | 
+            Q(tarea__nombre__icontains=q)
+        )
+    '''if filtro_estado == "pendiente":
         asignaciones = AlumnoTarea.objects.filter(estado="pendiente")
     elif filtro_estado == "terminada":
         asignaciones = AlumnoTarea.objects.filter(estado="terminada")
     else:
-        asignaciones = AlumnoTarea.objects.all()
+        asignaciones = AlumnoTarea.objects.all()'''
 
     alumnos_con_tareas = {}
+    for asig in asignaciones.order_by('alumno__nombre'):
+        aid = asig.alumno.id
+        if aid not in alumnos_con_tareas:
+            alumnos_con_tareas[aid] = {
+                'alumno': asig.alumno,
+                'tareas': []
+            }
+        alumnos_con_tareas[aid]['tareas'].append(asig)
+        
+        alumnos_list= Alumnos.objects.all().order_by('nombre')
+        return render(request, "registros/principal.html", {
+        "alumnos_con_tareas": alumnos_con_tareas,
+        "filtro_estado": filtro_estado,
+        "alumnos_list": alumnos_list,
+        "alumno_seleccionado": alumno_id,
+        "q": q,
+    })
+    '''alumnos_con_tareas = {}
     for asignacion in asignaciones:
         alumno_id = asignacion.alumno.id
         if alumno_id not in alumnos_con_tareas:
@@ -30,7 +67,7 @@ def registros(request):
     return render(request, "registros/principal.html", {
         "alumnos_con_tareas": alumnos_con_tareas,
         "filtro_estado": filtro_estado
-    })
+    })'''
 
 def entregar_tarea(request, tarea_id):
     asignacion = get_object_or_404(AlumnoTarea, id=tarea_id)
